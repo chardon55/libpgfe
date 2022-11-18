13c56beff78725ed87a6d1f575e49eb93287761b
chardon55 <charlesdong_2000@outlook.com>
2022-11-18 16:08:11 +0800

    Update functions

--- src/c/rijndael/rijndael.c
+++ src/c/rijndael/rijndael.c
@@ -8,3 +8,11 @@
 */
 
 #include "rijndael.h"
+
+static inline void __sub_bytes(struct pgfe_rijnael_ctx *ctx) {}
+
+static inline void __shift_rows(struct pgfe_rijnael_ctx *ctx) {}
+
+static inline void __mix_columns(struct pgfe_rijnael_ctx *ctx) {}
+
+static inline void __add_round_key(struct pgfe_rijnael_ctx *ctx, pgfe_rijndael_word_t w[]) {}
